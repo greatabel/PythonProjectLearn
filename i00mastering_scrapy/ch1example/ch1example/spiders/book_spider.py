66edfafa7b48bb92368424b722d1b027a510b328
greatabel <myreceiver2for2github@gmail.com>
2018-08-15 00:07:21 +0800

    book scrapy

--- i00mastering_scrapy/ch1example/ch1example/spiders/book_spider.py
+++ i00mastering_scrapy/ch1example/ch1example/spiders/book_spider.py
@@ -5,4 +5,18 @@ class BooksSpider(scrapy.Spider):
     name = 'books'
 
     start_urls = ['http://books.toscrape.com/']
-    
+    def parse(self, response):
+        for book in response.css('article.product_pod'):
+            name = book.xpath('./h3/a/@title').extract_first()
+            price = book.css('p.price_color::text').extract_first()
+            yield {'name': name, 'price': price}
+            
+        next_url = response.css('ul.pager li.next a::attr(href)').extract_first()
+        if next_url:
+            # 如果找到下一页的URL，得到绝对路径，构造新的Request 对象
+            next_url = response.urljoin(next_url)
+            yield scrapy.Request(next_url, callback=self.parse)
+
+
+
+
