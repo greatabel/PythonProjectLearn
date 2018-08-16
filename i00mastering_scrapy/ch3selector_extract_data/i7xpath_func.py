36535b0e14f7ad26405e0c0f00129e456a6cf99d
greatabel <myreceiver2for2github@gmail.com>
2018-08-16 22:52:03 +0800

    string

--- i00mastering_scrapy/ch3selector_extract_data/i7xpath_func.py
+++ i00mastering_scrapy/ch3selector_extract_data/i7xpath_func.py
@@ -4,9 +4,14 @@ import mycolor
 
 
 text_content = '<a href="#">click here to go to the \
-        <strong>Next Page</strong></a>'
+<strong>Next Page</strong></a>'
 sel = Selector(text=text_content)
 print(sel)
 print(sel.xpath('/html/body/a/strong/text()'))
 print(mycolor.show('same as 使用string函数:'), 
     sel.xpath('string(/html/body/a/strong)').extract())
+
+print('如果想得到a 中的整个字符串Click here to go to the Next Page')
+print(sel.xpath('/html/body/a//text()'))
+
+print(sel.xpath('string(/html/body/a)').extract())
