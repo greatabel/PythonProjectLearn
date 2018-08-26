bdedcff77e806dc38c04fd310eae4d3a5f36d0a6
greatabel <myreceiver2for2github@gmail.com>
2018-08-26 22:31:24 +0800

    i0BookItem.py

---
+++ i00mastering_scrapy/ch4Item_to_seal_data/i0BookItem.py
@@ -0,0 +1,5 @@
+from scrapy import Item, Field
+
+class BookItem(Item):
+    name = Field()
+    price = Field()
---
+++ i00mastering_scrapy/ch4Item_to_seal_data/i1client_of_i0.py
@@ -0,0 +1,5 @@
+import i0BookItem
+
+
+book1 = i0BookItem.BookItem(name="Zero to One", price=99.9)
+print(book1)
