b2b9881b0294f6cad70f5efaffcb229ffefdd7bb
greatabel <myreceiver2for2github@gmail.com>
2018-08-26 22:16:26 +0800

    E1 E2   E1>E2 [ATTR]

--- i00mastering_scrapy/ch3selector_extract_data/i9css_selector.py
+++ i00mastering_scrapy/ch3selector_extract_data/i9css_selector.py
@@ -14,7 +14,7 @@ body = '''
                     <a href='image2.html'>Name: Image 2 <br/><img src='image2.jpg' /></a>
                     <a href='image3.html'>Name: Image 3 <br/><img src='image3.jpg' /></a>
                 </div>
-                       <div id='images-2' class='small'>
+                <div id='images-2' class='small'>
                     <a href='image4.html'>Name: Image 4 <br/><img src='image4.jpg' /></a>
                     <a href='image5.html'>Name: Image 5 <br/><img src='image5.jpg' /></a>
                 </div>
@@ -25,4 +25,7 @@ body = '''
 response = HtmlResponse(url='http://www.example.com', body=body, encoding='utf8')
 print('“选中所有的img=>”', response.css('img'))
 print('“选中所有base和title”', response.css('base, title'))
+print('“E1 E2：选中E1后代元素中的E2元素”:', response.css('div img'))
+print('“E1>E2：选中E1子元素中的E2元素”:', response.css('body>div'))
+print('“[ATTR]：选中包含ATTR属性的元素”', response.css('[style]'))
 
