664179931c6023c341d54b4f5fcd4f07201a8212
greatabel <myreceiver2for2github@gmail.com>
2018-09-13 18:17:13 +0800

    deny parameter in LinkExtractor

--- i00mastering_scrapy/ch6LinkExtractor/i1extract_rule.py
+++ i00mastering_scrapy/ch6LinkExtractor/i1extract_rule.py
@@ -1,4 +1,6 @@
 from scrapy.http import HtmlResponse
+from scrapy.linkextractors import LinkExtractor
+from urllib.parse import urlparse
 import mycolor
 
 
@@ -8,7 +10,7 @@ response1 = HtmlResponse(url='http://example1.com', body=html1, encoding='utf8')
 response2 = HtmlResponse(url='http://example2.com', body=html2, encoding='utf8')
 
 
-from scrapy.linkextractors import LinkExtractor
+
 le = LinkExtractor()
 links = le.extract_links(response1)
 for link in links:
@@ -17,6 +19,15 @@ for link in links:
 print(mycolor.show('allow 参数 --------'))
 pattern = '/intro/.+\.html$'
 le = LinkExtractor(allow=pattern)
+links = le.extract_links(response1)
+for link in links:
+    print(link)
+
+print(mycolor.show('deny 参数 --------'))
+print('“示例　提取页面example1.html中所有站外链接（即排除站内链接）”')
+pattern1 = pattern1 = '^' + urlparse(response1.url).geturl()
+print(pattern1)
+le = LinkExtractor(deny=pattern1)
 links = le.extract_links(response1)
 for link in links:
     print(link)
