605dfb908f581935db199b77e30c62ac6e4d1276
greatabel <myreceiver2for2github@gmail.com>
2019-05-28 01:11:20 +0800

    cookie

---
+++ i00mastering_scrapy/ch10simulate_login/browser-cookie3-demo/demo.py
@@ -0,0 +1,13 @@
+import browser_cookie3
+
+
+# 获取Chrome 浏览器中的Cookie
+chrome_cookiejar = browser_cookie3.chrome()        
+firefox_cookiejar = browser_cookie3.firefox()  # 获取Firefox 浏览器中的Cookie
+print(type(chrome_cookiejar), '#'*10, type(firefox_cookiejar))
+print(len(chrome_cookiejar))
+count = 0
+for cookie in chrome_cookiejar:
+    count += 1
+    if count <= 10:
+        print(cookie)
