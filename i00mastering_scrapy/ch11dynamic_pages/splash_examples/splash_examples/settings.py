30b3d1c06a01bbb27f7c7969e7f1a7c0291d3d8b
greatabel <myreceiver2for2github@gmail.com>
2019-05-29 23:28:21 +0800

    setting of splash

--- i00mastering_scrapy/ch11dynamic_pages/splash_examples/splash_examples/settings.py
+++ i00mastering_scrapy/ch11dynamic_pages/splash_examples/splash_examples/settings.py
@@ -21,6 +21,10 @@ NEWSPIDER_MODULE = 'splash_examples.spiders'
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
 
+# Splash服务器地址
+SPLASH_URL = 'http://localhost:8050'
+
+
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
 
@@ -52,9 +56,20 @@ ROBOTSTXT_OBEY = True
 
 # Enable or disable downloader middlewares
 # See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    'splash_examples.middlewares.SplashExamplesDownloaderMiddleware': 543,
-#}
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy_splash.SplashCookiesMiddleware': 723,
+    'scrapy_splash.SplashMiddleware': 725,
+    'scrapy.downloadermiddlewares.httpcompression.HttpCompressionMiddleware': 810,
+}
+
+# 设置去重过滤器
+DUPEFILTER_CLASS = 'scrapy_splash.SplashAwareDupeFilter'
+
+# 用来支持cache_args（可选）
+SPIDER_MIDDLEWARES = {
+    'scrapy_splash.SplashDeduplicateArgsMiddleware': 100,
+}
+
 
 # Enable or disable extensions
 # See https://doc.scrapy.org/en/latest/topics/extensions.html
