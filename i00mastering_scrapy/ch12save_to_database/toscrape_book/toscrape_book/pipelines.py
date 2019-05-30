359d87b24252c7a4a76c97e1a2a79a6f567e92e1
greatabel <myreceiver2for2github@gmail.com>
2019-05-30 17:26:33 +0800

    add func in pipeline.py

--- i00mastering_scrapy/ch12save_to_database/toscrape_book/toscrape_book/pipelines.py
+++ i00mastering_scrapy/ch12save_to_database/toscrape_book/toscrape_book/pipelines.py
@@ -5,6 +5,8 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+import sqlite3
+
 
 class ToscrapeBookPipeline(object):
     def process_item(self, item, spider):
@@ -24,3 +26,41 @@ class BookPipeline(object):
         if rating:
             item['review_rating'] = self.review_rating_map[rating]
         return item
+
+
+class SQLitePipeline(object):
+  
+  def open_spider(self, spider):
+     db_name = spider.settings.get('SQLITE_DB_NAME', 'scrapy_defaut.db')
+     self.db_conn = sqlite3.connect(db_name)
+     self.db_cur = self.db_conn.cursor()
+
+  def close_spider(self, spider):
+     self.db_conn.commit()
+     self.db_conn.close()
+
+  def close_spider(self, spider):
+     self.db_conn.commit()
+     self.db_conn.close()
+
+  def process_item(self, item, spider):
+     self.insert_db(item)
+     return item
+
+  def insert_db(self, item):
+     values = (
+        item['upc'],
+        item['name'],
+        item['price'],
+        item['review_rating'],
+        item['review_num'],
+        item['stock'],
+     )
+     sql = 'INSERT INTO books VALUES (?,?,?,?,?,?)'
+     self.db_cur.execute(sql, values)
+     # 每插入一条就commit一次会影响效率
+     # self.db_conn.commit()
+    # 每插入一条就commit一次会影响效率
+     # self.db_conn.commit()
+
+
