1ad135c8b83693746353160d1333a6b99abb7bc8
greatabel <myreceiver2for2github@gmail.com>
2020-03-01 10:58:38 +0800

    with parameter

--- i01Python-API-Development-Fundamentals/ch1first_step/i1app.py
+++ i01Python-API-Development-Fundamentals/ch1first_step/i1app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, request
-
+from http import HTTPStatus
 
 app = Flask(__name__)
 
@@ -20,6 +20,14 @@ recipes = [
 def get_recipes():
     return jsonify({'data': recipes})
 
+@app.route("/recipes/<int:recipe_id>", methods=['GET'])
+def get_recipe(recipe_id):
+    recipe = next((recipe for recipe in recipes if recipe['id'] == recipe_id), None)
+
+    if recipe:
+        return jsonify(recipe)
+
+    return jsonify({'message': 'recipe not found'}), HTTPStatus.NOT_FOUND
 
 if __name__ == "__main__":
     app.run()
