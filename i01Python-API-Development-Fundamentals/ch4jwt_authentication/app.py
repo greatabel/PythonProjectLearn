955f79eba42cd742ddae6fbadebaaf05f2a6ae77
greatabel <myreceiver2for2github@gmail.com>
2020-03-04 15:02:31 +0800

    me endpooint

--- i01Python-API-Development-Fundamentals/ch4jwt_authentication/app.py
+++ i01Python-API-Development-Fundamentals/ch4jwt_authentication/app.py
@@ -5,7 +5,7 @@ from flask_restful import Api
 from config import Config
 from extensions import db, jwt
 
-from resources.user import UserListResource, UserResource
+from resources.user import UserListResource, UserResource, MeResource
 from resources.token import TokenResource
 from resources.recipe import RecipeListResource, RecipeResource, RecipePublishResource
 
@@ -31,6 +31,8 @@ def register_resources(app):
     api.add_resource(UserListResource, '/users')
     api.add_resource(UserResource, '/users/<string:username>')
 
+    api.add_resource(MeResource, '/me')
+    
     api.add_resource(TokenResource, '/token')
     
     api.add_resource(RecipeListResource, '/recipes')
--- i01Python-API-Development-Fundamentals/ch4jwt_authentication/resources/user.py
+++ i01Python-API-Development-Fundamentals/ch4jwt_authentication/resources/user.py
@@ -1,6 +1,6 @@
 from flask import request
 from flask_restful import Resource
-from flask_jwt_extended import jwt_optional, get_jwt_identity
+from flask_jwt_extended import jwt_optional, get_jwt_identity, jwt_required
 from http import HTTPStatus
 
 from utils import hash_password
@@ -69,4 +69,18 @@ class UserResource(Resource):
         return data, HTTPStatus.OK
 
 
+class MeResource(Resource):
+
+    @jwt_required
+    def get(self):
+        user = User.get_by_id(id=get_jwt_identity())
+
+        data = {
+            'id': user.id,
+            'username': user.username,
+            'email': user.email,
+        }
+
+        return data, HTTPStatus.OK
+        
 
