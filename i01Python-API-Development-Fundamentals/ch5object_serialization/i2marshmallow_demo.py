3f0a6f71d51d6457eb5ec78a198da8c48660b89b
greatabel <myreceiver2for2github@gmail.com>
2020-03-06 15:08:05 +0800

    i2 loads

--- i01Python-API-Development-Fundamentals/ch5object_serialization/i2marshmallow_demo.py
+++ i01Python-API-Development-Fundamentals/ch5object_serialization/i2marshmallow_demo.py
@@ -1,5 +1,5 @@
 import datetime as dt
-from marshmallow import Schema, fields
+from marshmallow import Schema, fields, post_load
 from marshmallow import pprint
 
 
@@ -36,3 +36,42 @@ print('You can also serialize to a JSON-encoded string: 注意dump vs dumps')
 json_result = schema.dumps(user)
 pprint(json_result)
 print(type(json_result))
+
+user1 = User(name="Mick", email="mick@stones.com")
+user2 = User(name="Keith", email="keith@stones.com")
+users = [user1, user2]
+schema = UserSchema(many=True)
+result = schema.dump(users)  # OR UserSchema().dump(users, many=True)
+pprint(result)
+
+
+print('Deserializing Objects\n')
+user_data = {
+    # "created_at": "2014-08-11T05:26:03.869245",
+    "email": "ken@yahoo.com",
+    "name": "Ken",
+}
+schema = UserSchema()
+result = schema.load(user_data)
+pprint(result)
+print(type(result))
+
+class UserSchemaB(Schema):
+    name = fields.Str()
+    email = fields.Email()
+    # created_at = fields.DateTime()
+
+    @post_load
+    def make_user(self, data, **kwargs):
+        return User(**data)
+
+schema_b = UserSchemaB()
+result = schema_b.load(user_data)
+pprint(result)
+print(type(result))
+
+
+
+
+
+
