23b3be5ba3f3e2879bbb9b1e14f722185fc31e9a
greatabel <myreceiver2for2github@gmail.com>
2020-03-31 11:07:50 +0800

    mv to FlaskRepository

--- i01Python-API-Development-Fundamentals/ch5object_serialization/utils.py
+++
@@ -1,9 +0,0 @@
-from passlib.hash import pbkdf2_sha256
-
-
-def hash_password(pasword):
-    return pbkdf2_sha256.hash(pasword)
-
-
-def check_password(pasword, hashed):
-    return pbkdf2_sha256.verify(pasword, hashed)
--- i01Python-API-Development-Fundamentals/ch8pgination_search_order/extensions.py
+++
@@ -1,9 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask_jwt_extended import JWTManager
-from flask_uploads import UploadSet, IMAGES
-
-
-db = SQLAlchemy()
-jwt = JWTManager()
-# https://github.com/maxcountryman/flask-uploads/blob/master/flask_uploads.py
-image_set = UploadSet('images', IMAGES)
