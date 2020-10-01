6afcaf1c42288899a23b89ded7d57eb517aade42
greatabel <myreceiver2for2github@gmail.com>
2020-10-01 12:44:01 +0800

    template and css

--- b5793pay_integration/app.py
+++ b5793pay_integration/app.py
@@ -1,10 +1,21 @@
 from flask import Flask
 from flask import render_template
+import json
 
 
 app = Flask(__name__)
+app.debug = True
+
+def get_products():
+    print('get_products', '#'*10)
+    # Reading data 
+    with open('products.json', 'r') as f:
+        products = json.load(f)
+        return products
+    return None
 
 
 @app.route('/')
 def cart_list():
-    return render_template('index.html')
+    products = get_products()
+    return render_template('index.html', products=products)
