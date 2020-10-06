8107ce404f5ae5d0ca006d81067b5e51ef4fb134
greatabel <myreceiver2for2github@gmail.com>
2020-10-06 18:57:53 +0800

    model

--- a6053tpcc_django/django_line/AppLine/views.py
+++ a6053tpcc_django/django_line/AppLine/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import psycopg2
 
+# conn = psycopg2.connect(database="TPCC", user="postgres", password="postgres", host="localhost", port="5432")
+# print("Opened database successfully")
 
 def index(request):
     # return HttpResponse("Hello, world. You're at the polls index.")
--- a6053tpcc_django/django_line/django_line/settings.py
+++ a6053tpcc_django/django_line/django_line/settings.py
@@ -75,13 +75,32 @@ WSGI_APPLICATION = 'django_line.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES = {
+
+    'default': {
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'TPCC',
+
+        'USER': 'postgres',
+
+        'PASSWORD': 'postgres',
+
+        'HOST': '127.0.0.1',
+
+        'PORT': '5432',
+
+    }
+
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
