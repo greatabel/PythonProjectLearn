8722ac8297eaf04fabf18d7f4495615d04de1124
greatabel <myreceiver2for2github@gmail.com>
2020-10-06 18:17:17 +0800

    add html

--- a6053tpcc_django/django_line/AppLine/views.py
+++ a6053tpcc_django/django_line/AppLine/views.py
@@ -1,5 +1,13 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    # return HttpResponse("Hello, world. You're at the polls index.")
+    context = {
+        'test': 'test',
+
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'AppLine/index.html', context=context)
