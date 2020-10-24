961fd71a0615983fd91b156ea3acb804afe27070
greatabel <myreceiver2for2github@gmail.com>
2020-10-25 04:29:23 +0800

    fix test

--- a6572movie_webapp/tests/e2e/test_web_app.py
+++ a6572movie_webapp/tests/e2e/test_web_app.py
@@ -5,7 +5,7 @@ from flask import session
 
 def test_movies_with_pagenum(client):
     # Check that we can retrieve the articles page.
-    response = client.get('/1')
+    response = client.get('/home/1')
     assert response.status_code == 200
     # Check that all articles on the requested date are included on the page.
     assert b'Avengers' in response.data
@@ -13,7 +13,7 @@ def test_movies_with_pagenum(client):
 
 def test_movies_with_error_pagenum(client):
     # Check that we can retrieve the articles page.
-    response = client.get('/10')
+    response = client.get('/home/10')
     assert response.status_code == 200
     # Check that all articles on the requested date are included on the page.
     assert b'Avengers' not in response.data
@@ -21,13 +21,13 @@ def test_movies_with_error_pagenum(client):
 
 def test_movies_without_pagenum(client):
 
-    response = client.get('/')
+    response = client.get('/home')
     assert response.status_code == 404
 
 
 def test_movies_post_pagenum(client):
     response = client.post(
-        '/1',
+        '/home/1',
         data={'username': 't'}
     )
     assert response.status_code == 405
