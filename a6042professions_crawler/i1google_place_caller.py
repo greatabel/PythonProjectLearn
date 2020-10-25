52e868f4e00a413cb101bdb0b5ff6f3a23f9ec8b
greatabel <myreceiver2for2github@gmail.com>
2020-10-26 01:40:06 +0800

    add page

--- a6042professions_crawler/i1google_place_caller.py
+++ a6042professions_crawler/i1google_place_caller.py
@@ -1,6 +1,6 @@
 import googlemaps
 from os import environ
-
+import time
 
 api_key = environ.get('GOOGLE_CLOUD_API_KEY', '')
 gmaps = googlemaps.Client(key=api_key)
@@ -8,9 +8,42 @@ gmaps = googlemaps.Client(key=api_key)
 
 destination = 'LakeWorth'
 
-mymakets = ['Barber', 'Beautician', 'Make up Artist',
- 'Nail tech(Nail artist) Esthetician', 'Massage therapists']
-local = gmaps.places(mymakets[0] + ' near ' + destination)
+mymakets_A = ['Barber', 'Beautician', 'Make up Artist',
+ "Nail tech(Nail artist) Esthetician'", 'Massage therapist']
 
-t0 = local['results']
-print(t0)
+mymakets_B = ['Beautician', 'Make up Artist',
+ "Nail tech(Nail artist)"]
+
+
+# def get_places():
+#     local = gmaps.places(mymakets[0] + ' near ' + destination)
+#     # https://developers.google.com/places/supported_types
+#     #gmaps.places(location=(lat,lng), type="movie_theater")
+#     # print(local)
+#     print('\n'*3)
+#     t0 = local['results']
+#     print(t0)
+
+
+def printHotels(searchString, next=''):
+    try:
+        places_result = gmaps.places(query=searchString, page_token=next)
+    except ApiError as e:
+        print(e)
+    else:
+        for result in places_result['results']:
+            print(result['name'], '#'*10)
+            print(result)
+    time.sleep(2)
+    try:
+        places_result['next_page_token']
+        print('-'*10, places_result['next_page_token'])
+    except KeyError as e:
+        print('Complete')
+    else:
+        printHotels(searchString, next=places_result['next_page_token'])
+
+
+
+if __name__ == "__main__":
+    printHotels(mymakets_A[0] + ' near ' + destination)
