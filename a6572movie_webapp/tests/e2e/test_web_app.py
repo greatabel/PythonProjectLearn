3167f923559625f6bced2c2fcf435b075b0e3afa
greatabel <myreceiver2for2github@gmail.com>
2020-10-25 08:33:24 +0800

    test

--- a6572movie_webapp/tests/e2e/test_web_app.py
+++ a6572movie_webapp/tests/e2e/test_web_app.py
@@ -3,10 +3,20 @@ import pytest
 from flask import session
 
 
-def test_movies_with_pagenum(client):
+def test_login_without_auth(client):
     # Check that we can retrieve the articles page.
     response = client.post('/login')
     assert response.status_code == 404
 
 
+def test_logout_without_auth(client):
+    # Check that we can retrieve the articles page.
+    response = client.post('/logout')
+    assert response.status_code == 404
 
+
+
+def test_home_without_auth(client):
+    # Check that we can retrieve the articles page.
+    response = client.post('/home/1?keyword=test')
+    assert response.status_code == 405
