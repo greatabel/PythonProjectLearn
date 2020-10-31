16c8328921762e9ad638479d555bde30e662abb1
greatabel <myreceiver2for2github@gmail.com>
2020-11-01 00:49:31 +0800

    different color and mark and size

--- a6808life_game/PaSciRoAnimal.py
+++ a6808life_game/PaSciRoAnimal.py
@@ -1,9 +1,12 @@
 class PaSciRoAnimal():
     myclass = "Animal"
+    shape_on_plot = None
 
-    def __init__(self, normal_speed, colour): 
+    def __init__(self, normal_speed, colour='yellow', size=2): 
         self.normal_speed = normal_speed
         self.colour = colour
+        # size of the body
+        self.size = size
 
 
     def __str__(self):
@@ -12,16 +15,18 @@ class PaSciRoAnimal():
     def printit(self):
         spacing = 20 - len(self.myclass)
         print(self.myclass.upper(), spacing*' ' +
-              ': normal_speed', self.normal_speed,'\tColour: ', self.colour)
+              ': normal_speed', self.normal_speed,'\tColour: ', self.colour,
+              '\size: ',self.size)
 
 
 class PaSciRoTiger(PaSciRoAnimal):
     myclass = "PaSciRoTiger"
-
+    shape_on_plot = "H"
 
 class PaSciRoSheep(PaSciRoAnimal):
     myclass = "PaSciRoSheep"
-
+    shape_on_plot = "P"
 
 class PaSciRoBird(PaSciRoAnimal):
     myclass = "PaSciRoSheepBird"
+    shape_on_plot = "^"
--- a6808life_game/pasciro_life_simulation.py
+++ a6808life_game/pasciro_life_simulation.py
@@ -26,44 +26,71 @@ XMAX  = 200
 YMAX  = 100
 POP   = 20
 STEPS = 10
-STEPS = 2
+STEPS = 5
     
 def main():
-    tiger = PaSciRoTiger(5, 'yellow')
+    # init 3 Alien Species: PaSciRoTiger PaSciRoSheep PaSciRoBird
+    tiger = PaSciRoTiger(5, 'yellow', 120)
     tiger.printit()
-    sheep = PaSciRoSheep(2, 'blue')
+    sheep = PaSciRoSheep(2, 'blue', 60)
     sheep.printit()
-    bird = PaSciRoBird(8, 'red')
+    bird = PaSciRoBird(8, 'red',30)
     bird.printit()
+    alien_species = {0: tiger, 1: sheep, 2: bird}
+    # inital population of anmial:
+    # size is bigger, the num of species should be smaller
+    # so we initally with less tiger, more sheep, even more birds
+    pops = [POP//8, POP//4, POP*5 //8]
+    for key, animal_obj in alien_species.items():
 
-    lifeforms = np.zeros((POP, 3), dtype=int)
-    print('0 lifeforms=', lifeforms)
-    for i in range(POP):
-        randX = random.randint(0,XMAX)
-        randY = random.randint(0,YMAX)
-        randTYPE = random.randint(0,2)
-        lifeforms[i,0] = randX
-        lifeforms[i,1] = randY
-        lifeforms[i,2] = randTYPE
-        print(lifeforms[i])
-    print('1 lifeforms=', lifeforms)
-    for i in range(STEPS):
-        print("\n ### TIMESTEP ",i, "###")
-        xvalues = []
-        yvalues = []
-        colours = []
-        for l in lifeforms:
-            stepChange(l)
-            #print(l)
-            xvalues.append(l[0])
-            yvalues.append(l[1])
-            colours.append(l[2])
-        
-        plt.scatter(xvalues, yvalues, c=colours)   # Note plt origin is bottom left 
-        plt.xlim(0,XMAX)
-        plt.ylim(0,YMAX)
-        plt.show()
-        time.sleep(2)
-    print('2 lifeforms=', lifeforms)   
+        x = np.random.randint(XMAX, size=pops[key])
+        y = np.random.randint(YMAX, size=pops[key])
+        plt.scatter(x, y, c =animal_obj.colour,  
+            linewidths = 1,  
+            marker = animal_obj.shape_on_plot,  
+            edgecolor ="green",  
+            s = animal_obj.size)
+
+      
+    plt.xlabel("X-axis") 
+    plt.ylabel("Y-axis") 
+    plt.show() 
+    # lifeforms = np.zeros((POP, 3), dtype=int)
+    # print('0 lifeforms=', lifeforms)
+    # for i in range(POP):
+    #     randX = random.randint(0,XMAX)
+    #     randY = random.randint(0,YMAX)
+    #     randTYPE = random.randint(0,2)
+    #     lifeforms[i,0] = randX
+    #     lifeforms[i,1] = randY
+    #     lifeforms[i,2] = randTYPE
+    #     print(lifeforms[i])
+    # print('1 lifeforms=', lifeforms)
+    # for i in range(STEPS):
+    #     print("\n ### TIMESTEP ",i, "###")
+    #     xvalues = []
+    #     yvalues = []
+    #     colours = []
+    #     for l in lifeforms:
+    #         print('l=', l)
+    #         print('l[2]=', l[2])
+    #         stepChange(l)
+    #         #print(l)
+    #         xvalues.append(l[0])
+    #         yvalues.append(l[1])
+    #         colours.append(l[2])
+    #     print('colours=', colours)
+    #     plt.scatter(xvalues, yvalues, c=colours)   # Note plt origin is bottom left 
+    #     plt.xlim(0,XMAX)
+    #     plt.ylim(0,YMAX)
+
+    #     plt.show(block=False)
+    #     # time.sleep(1)
+    #     plt.pause(1)
+    #     plt.close()
+    # print('2 lifeforms=', lifeforms)
+
+
 if __name__ == "__main__":
     main()
+
