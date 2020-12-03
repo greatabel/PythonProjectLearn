1e0989a473af95e9480d1b81493a068e3f79bfac
greatabel <myreceiver2for2github@gmail.com>
2020-12-03 21:04:52 +0800

    move get_db_data into api

--- a7990_OrganizationChart/wsgi.py
+++ a7990_OrganizationChart/wsgi.py
@@ -29,19 +29,21 @@ def get_db_data(table_number):
     elif table_number == 2:
         result = db.session.execute('SELECT gsmc, zgbl, xjgs, xjzg FROM uf_tzgs' )
     elif table_number == 3:
-        result = db.session.execute('SELECT labelid, sType FROM ecology.accountmoremenuinfo' )
+        result = db.session.execute('SELECT labelid, sType FROM accountmoremenuinfo' )
     for r in result:
         # print(r[0], '#' , r[1]) # Access by positional index
         # print(r['sType']) # Access by column name as a string
         r_dict = dict(r.items())
         print(r_dict)
 
-print('-'*10, 'start')
-get_db_data(3)
-print('-'*10, 'end')
+
 
 @app.route("/org_chart", methods=["GET", "POST"])
 def org_chart():
+    print('-'*10, 'start')
+    get_db_data(3)
+    print('-'*10, 'end')
+    
     charts = []
     return render_template(
         'org_chart.html',
