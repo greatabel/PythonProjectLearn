8a60c21b58ef2effdfc017803382f353b1157934
greatabel <myreceiver2for2github@gmail.com>
2020-12-17 02:58:58 +0800

    encryption

--- b8275_file_share/main.py
+++ b8275_file_share/main.py
@@ -8,9 +8,16 @@ def parser():
         "--ip",
         type=str,
         default="127.0.0.1",
-        help="ip list of servers",
+        help="target ip list of servers",
+    )
+    parser.add_argument(
+        "--encryption",
+        type=str,
+        default="no",
+        help="encryption yes/no",
     )
     args = parser.parse_args()
+    print(args.ip, args.encryption)
     iplist_str = args.ip
     
     return iplist_str.split(',')
@@ -23,3 +30,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+    '''
+    python3 main.py --ip 192.168.0.1,192.168.0.2 --encryption yes
+
+    '''
