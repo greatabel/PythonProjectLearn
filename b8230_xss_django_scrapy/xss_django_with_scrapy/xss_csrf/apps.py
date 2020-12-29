41c47ece5eab26a3324529262163ed33099a0e1f
greatabel <myreceiver2for2github@gmail.com>
2020-12-29 16:47:20 +0800

    app0

--- b8230_xss_django_scrapy/xss_django_with_scrapy/xss_csrf/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class XssCsrfConfig(AppConfig):
-    name = 'xss_csrf'
--- b8230_xss_django_scrapy/xss_django_with_scrapy/xss_django_with_scrapy/urls.py
+++ b8230_xss_django_scrapy/xss_django_with_scrapy/xss_django_with_scrapy/urls.py
@@ -1,23 +1,26 @@
-"""xss_django_with_scrapy URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from django.conf.urls import url,include
 from django.contrib import admin
-from django.urls import include, path
+from app01 import views
 
 urlpatterns = [
-    path('xss_csrf/', include('xss_csrf.urls')),
 
-    path('admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
+    # url(r'^index/$', views.index),
+    url(r'^login/$', views.login),
+    url(r'^register/$', views.register),
+    url(r'^logout/', views.logout),
+    url(r"^captcha/",include('captcha.urls')),
+    url(r"^showbooks/$", views.showbooks,name="showbooks"),
+    url(r"^addbook/$", views.addbook,name="addbook"),
+    url(r"^delbook/$", views.delbook,name="delbook"),
+    url(r"^editbook/(\d+)/$", views.editbook,name="editbook"),
+
+
+
 ]
+'''
+未登录人员，不论是访问index还是login和logout，全部跳转到login界面
+已登录人员，访问login会自动跳转到index页面
+已登录人员，不允许直接访问register页面，需先logout
+登出后，自动跳转到login界面
+'''
