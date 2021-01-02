e11fd59b71c5d301ce4a556403e19446aa327d05
greatabel <myreceiver2for2github@gmail.com>
2021-01-02 17:17:05 +0800

    done with pages

--- b8230_xss_django_scrapy/scrapy_securityweb/scrapy_securityweb/spiders/securityweb_spider.py
+++ b8230_xss_django_scrapy/scrapy_securityweb/scrapy_securityweb/spiders/securityweb_spider.py
@@ -2,17 +2,28 @@ import scrapy
 
 class SecurityWebSpider(scrapy.Spider):
 
+    def __init__(self):
+        self.page_index = 1
+
+
     name = 'securityweb'
-    # start_urls = ['https://www.cert.org.cn/publish/main/9/index.html']
-    start_urls = ['http://books.toscrape.com/']
+    start_urls = ['http://hackernews.cc/']
+    # start_urls = ['https://www.cert.org.cn/publish/main/12/index.html']
+    # start_urls = ['http://books.toscrape.com/']
     def parse(self, response):
-        for book in response.css('article.product_pod'):
-            name = book.xpath('./h3/a/@title').extract_first()
-            price = book.css('p.price_color::text').extract_first()
+        for book in response.css('classic-list'):
+            name = book.css('classic-list-title a::attr(href)').extract_first()
+            price = book.css('light-post-meta').extract_first()
+            print('#'*20, name, price)
             yield {'name': name, 'price': price}
-            
-        next_url = response.css('ul.pager li.next a::attr(href)').extract_first()
-        if next_url:
+
+        # https://blog.csdn.net/webfullstack/article/details/104878396
+        self.page_index += 1
+        # next_url = response.css('pagination clearfix a::attr(href)').extract_first()
+        next_url = 'http://hackernews.cc/page/' + str(self.page_index)
+        # next_url = response.xpath(PAGER_SELECTOR).extract_first()
+        print('>'*10, next_url)
+        if next_url and self.page_index <=10:
             # 如果找到下一页的URL，得到绝对路径，构造新的Request 对象
             next_url = response.urljoin(next_url)
             yield scrapy.Request(next_url, callback=self.parse)
