cb194f6541816c32ab95cba32c98d3de5bd9b900
greatabel <myreceiver2for2github@gmail.com>
2021-01-17 10:25:15 +0800

    name

--- a9156_twitter_trend/i0preprocess.py
+++ a9156_twitter_trend/i0preprocess.py
@@ -10,9 +10,8 @@ data2019 = csv_reader("2019file.csv", "data")
 print(data2019[0], "#" * 10, data2019[1], "#" * 10, " \n", data2019[2])
 
 
-
-print('-'*10, 'tweets:')
-print(data2020[1][10], '\n', '#'*10, '\n', data2019[1][10])
+print("-" * 10, "tweets:")
+print(data2020[1][10], "\n", "#" * 10, "\n", data2019[1][10])
 
 
 print("\n1. Heat comparison")
@@ -20,7 +19,7 @@ print(len(data2020), " VS ", len(data2019))
 
 data2020full = csv_reader("2020_total_file.csv", "data")
 
-print('\n2. sentiment anlaysis')
+print("\n2. sentiment anlaysis")
 total_sentiment = 0
 
 num_positive = 0
@@ -29,12 +28,14 @@ num_nagtive = 0
 
 unwanted_chars = ".,-_ ()’"
 wordfreq = {}
+usernamefreq = {}
 
-split = 1001
+split = 101
 pick_twlist = data2020full[0::split]
 for tw in pick_twlist:
     text = tw[10]
-    print(text, '\n')
+    username = tw[8]
+    print(text, "\n@@@username=", username, "\n")
     words, sentiment_tw = anlaysis(text)
     print(sentiment_tw)
     total_sentiment += sentiment_tw
@@ -48,17 +49,31 @@ for tw in pick_twlist:
     for raw_word in words:
         word = raw_word.strip(unwanted_chars)
         if word not in wordfreq:
-            wordfreq[word] = 0 
+            wordfreq[word] = 0
         wordfreq[word] += 1
 
-print('tatal sentiment polarity:', total_sentiment)
-print('everaget sentiment polarity:', total_sentiment/len(pick_twlist))
-print('number of (positive VS neural VS nagtive):', 
-        num_positive*split, num_neural*split, num_nagtive*split)
+    if username not in usernamefreq:
+        usernamefreq[username] = 0
+    usernamefreq[username] += 1
 
-print('\n 3. Related words related to this topic')
+print("tatal sentiment polarity:", total_sentiment)
+print("everaget sentiment polarity:", total_sentiment / len(pick_twlist))
+print(
+    "number of (positive VS neural VS nagtive):",
+    num_positive * split,
+    num_neural * split,
+    num_nagtive * split,
+)
+
+print("\n 3. related words related to this topic")
 # print(wordfreq)
 a1_sorted_keys = sorted(wordfreq, key=wordfreq.get, reverse=True)
 for r in a1_sorted_keys:
     if wordfreq[r] > 1:
         print(r, wordfreq[r])
+
+print("\n 4. username often posts related topics")
+a2_sorted_keys = sorted(usernamefreq, key=usernamefreq.get, reverse=True)
+for r in a2_sorted_keys:
+    if usernamefreq[r] > 1:
+        print(r, usernamefreq[r])
