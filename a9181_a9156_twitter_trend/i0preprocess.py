f3884bb9af15a9de8ecb0b6e29ae43b7652f47ac
greatabel <myreceiver2for2github@gmail.com>
2021-01-18 16:55:50 +0800

    change name

--- a9181_a9156_twitter_trend/i0preprocess.py
+++ a9181_a9156_twitter_trend/i0preprocess.py
@@ -25,6 +25,7 @@ print(len(data2020), " VS ", len(data2019))
 compare_txt = "'data2020':" + str(len(data2020)) + ", 'data2019':" + str(len(data2019))
 js_txt += compare_txt
 
+
 data2020full = csv_reader("2020_total_file.csv", "data")
 
 print("\n2. sentiment anlaysis")
@@ -34,8 +35,10 @@ num_positive = 0
 num_neural = 0
 num_nagtive = 0
 
-unwanted_chars = ".,-_ ()’"
+unwanted_chars = ".,-_ ()’'"
+black_list = ["sea https", "s", "u.s"]
 wordfreq = {}
+
 usernamefreq = {}
 
 split = 101
@@ -57,36 +60,64 @@ for tw in pick_twlist:
     for raw_word in words:
         word = raw_word.strip(unwanted_chars)
         if word not in wordfreq:
-            wordfreq[word] = 0
-        wordfreq[word] += 1
-
+            if word not in black_list:
+                wordfreq[word] = 0
+        if word not in black_list:
+            wordfreq[word] += 1
+    if username == 'Indo-Pacific News - Watching the CCP-China Threat':
+        username = 'Indo-Pacific News'
     if username not in usernamefreq:
         usernamefreq[username] = 0
     usernamefreq[username] += 1
 
 print("tatal sentiment polarity:", total_sentiment)
-print("everaget sentiment polarity:", total_sentiment / len(pick_twlist))
+print("average sentiment polarity:", total_sentiment / len(pick_twlist))
 print(
     "number of (positive VS neural VS nagtive):",
     num_positive * split,
     num_neural * split,
     num_nagtive * split,
 )
+sentiment_txt = ",'total_sentiment_polarity':" + str( round(total_sentiment, 2)) \
+    + ", 'average_sentiment_polarity':" + str(round( total_sentiment / len(pick_twlist),2))  \
+    + ",'num_positive':" + str(num_positive * split) + ", 'num_neural':" + str(num_neural * split) \
+     + ",'num_nagtive':" + str(num_nagtive * split) + ", 'data2019':" + str(len(data2019))
+js_txt += sentiment_txt
+
+js_txt += " };\n"
+
 
 print("\n 3. related words related to this topic")
+
+js_txt += 'var RELATED_WORDS = {'
 # print(wordfreq)
+index = 0
 a1_sorted_keys = sorted(wordfreq, key=wordfreq.get, reverse=True)
 for r in a1_sorted_keys:
     if wordfreq[r] > 1:
         print(r, wordfreq[r])
+        if index < 10:
+            js_txt += "'" + r + "':" + str(wordfreq[r]) + ','
+            index += 1
 
+
+js_txt += " };\n"
+
+
+index = 0
 print("\n 4. username often posts related topics")
+js_txt += 'var WHO_TWEETS = {'
 a2_sorted_keys = sorted(usernamefreq, key=usernamefreq.get, reverse=True)
 for r in a2_sorted_keys:
     if usernamefreq[r] > 1:
         print(r, usernamefreq[r])
+        if index < 10:
+            js_txt += "'" + r + "':" + str(usernamefreq[r]) + ','
+            index += 1
+
 
 js_txt += " };"
+
 # write to a local js file , let d3 do data-visual
 with open("data_visualization/data.js", 'w') as file:
     file.write(js_txt.strip())
