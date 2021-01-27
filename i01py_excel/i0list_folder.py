c5458b281286846a2cdc752d8662ed1ec02061a6
greatabel <myreceiver2for2github@gmail.com>
2021-01-27 14:39:44 +0800

    folder_test

--- i01py_excel/i0list_folder.py
+++ i01py_excel/i0list_folder.py
@@ -1,5 +1,20 @@
 import os
 
-path = '.'
+path = os.getcwd()
+print('path=', path)
+
+
 file_list = os.listdir(path)
 print(file_list)
+
+
+print('分离文件主名和扩展名')
+for f in file_list:
+    seperate = os.path.splitext(f)
+    print(seperate)
+
+
+print('重命名文件和文件夹')
+oldname = path + '/folder_test/test0.txt'
+newname = path + '/folder_test/changed0.txt'
+os.rename(oldname, newname)
