9eb34ba0493f393336f712d34b8aa525cb41f00e
greatabel <myreceiver2for2github@gmail.com>
2021-02-08 15:17:40 +0800

    additional csv

---
+++ a9351_weibo_spider/i3read_csv.R
@@ -0,0 +1,5 @@
+setwd("/Users/abel/Downloads/AbelProject/PythonProjectLearn/a9351_weibo_spider")
+
+# Import the data and look at the first six rows
+carSpeeds <- read.csv(file = 'data_results/i2user__202101290044.csv')
+head(carSpeeds)
