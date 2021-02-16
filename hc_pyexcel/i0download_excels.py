2564c883bc017502d8a236b4947ba3611664c1ce
greatabel <myreceiver2for2github@gmail.com>
2021-02-16 11:28:54 +0800

    i1

--- hc_pyexcel/i0download_excels.py
+++ hc_pyexcel/i0download_excels.py
@@ -1,6 +1,7 @@
 from os import environ
 
 import imaplib, email, os
+
 # import pandas as pd
 # import numpy as np
 from datetime import datetime
@@ -11,33 +12,35 @@ N = 5
 
 # https://zhuanlan.zhihu.com/p/94943212
 def fetch_attachment(inputmail):
-    #提取了指定编号（最新一封）的邮件
-    resp, data = conn.fetch (inputmail.split()[len(inputmail.split())-1],'(RFC822)')
+    # 提取了指定编号（最新一封）的邮件
+    resp, data = conn.fetch(inputmail.split()[len(inputmail.split()) - 1], "(RFC822)")
     emailbody = data[0][1]
     mail = email.message_from_bytes(emailbody)
 
-    fileName = '没有找到任何附件！' 
-    #获取邮件附件名称
+    fileName = "没有找到任何附件！"
+    # 获取邮件附件名称
     for part in mail.walk():
-        if part.get_content_maintype() == 'multipart':
+        if part.get_content_maintype() == "multipart":
             continue
-        if part.get('Content-Disposition') is None:
+        if part.get("Content-Disposition") is None:
             continue
-        fileName = part.get_filename() 
+        fileName = part.get_filename()
 
-    #如果文件名为纯数字、字母时不需要解码，否则需要解码
+        # 如果文件名为纯数字、字母时不需要解码，否则需要解码
         try:
-            fileName = decode_header(fileName)[0][0].decode(decode_header(fileName)[0][1])
+            fileName = decode_header(fileName)[0][0].decode(
+                decode_header(fileName)[0][1]
+            )
             # print(fileName, '#'*10)
         except:
             pass
-    #如果获取到了文件，则将文件保存在制定的目录下
-        if fileName != '没有找到任何附件！':
+        # 如果获取到了文件，则将文件保存在制定的目录下
+        if fileName != "没有找到任何附件！":
             # filePath = os.path.join("C:\\文件夹名称", fileName)
             filePath = path + fileName
 
             if not os.path.isfile(filePath):
-                fp = open(filePath, 'wb')
+                fp = open(filePath, "wb")
                 fp.write(part.get_payload(decode=True))
                 fp.close()
                 print("附件已经下载，文件名为：" + fileName)
@@ -47,38 +50,38 @@ def fetch_attachment(inputmail):
 
 if __name__ == "__main__":
     path = os.getcwd()
-    directory = (datetime.today()).strftime('%Y-%m-%d')   
+    directory = (datetime.today()).strftime("%Y-%m-%d")
     if not os.path.exists(directory):
         os.makedirs(directory)
-        
-    path = path + '/' + directory + '/'
-    #连接到qq企业邮箱，其他邮箱调整括号里的参数
+
+    path = path + "/" + directory + "/"
+    # 连接到qq企业邮箱，其他邮箱调整括号里的参数
     conn = imaplib.IMAP4_SSL("imap.exmail.qq.com", 993)
 
-    hengchang_qyweixin_username = environ.get('hengchang_qyweixin_username', 'username')
-    hengchang_qyweixin_password = environ.get('hengchang_qyweixin_password', 'password')
+    hengchang_qyweixin_username = environ.get("hengchang_qyweixin_username", "username")
+    hengchang_qyweixin_password = environ.get("hengchang_qyweixin_password", "password")
     # print(hengchang_qyweixin_username, hengchang_qyweixin_password)
 
-    #用户名、密码，登陆
+    # 用户名、密码，登陆
     conn.login(hengchang_qyweixin_username, hengchang_qyweixin_password)
     # conn.login("username","password")
 
-    #选定一个邮件文件夹
-    #可以用conn.list()查看都有哪些文件夹。中文的文件夹名称可能是乱码，没关系，直接拷贝过来就行了。
+    # 选定一个邮件文件夹
+    # 可以用conn.list()查看都有哪些文件夹。中文的文件夹名称可能是乱码，没关系，直接拷贝过来就行了。
     # print(conn.list())
 
     resp, mails = conn.select("INBOX")
     print(resp, mails)
     # https://stackoverflow.com/questions/52054196/python-imaplib-search-email-with-date-and-time
-    
+
     # total number of emails
     num_of_total_email = int(mails[0])
 
-    #提取了文件夹中所有邮件的编号，search功能在本邮箱中没有实现……
+    # 提取了文件夹中所有邮件的编号，search功能在本邮箱中没有实现……
     # resp, mails = conn.search(None,'ALL')
     counter = 0
-    for i in range(num_of_total_email, num_of_total_email-N, -1):
-        print('处理第', counter, '封邮件')
+    for i in range(num_of_total_email, num_of_total_email - N, -1):
+        print("处理第", counter, "封邮件")
         counter += 1
 
         fetch_attachment(str(i))
@@ -86,8 +89,7 @@ if __name__ == "__main__":
     conn.close()
     conn.logout()
 
-
-    #再用Pandas把数据读出来进行处理：
+    # 再用Pandas把数据读出来进行处理：
     # Data = pd.read_excel("C:\\文件夹名称\\" + fileName)
-    #此处省略一大堆的处理
-    #最后用df.to_excel()函数，把新的df保存就可以了。
+    # 此处省略一大堆的处理
+    # 最后用df.to_excel()函数，把新的df保存就可以了。
--- hc_pyexcel/i1process_excels.py
+++ hc_pyexcel/i1process_excels.py
@@ -1,17 +1,22 @@
 import imaplib, email, os
-# import pandas as pd
+import pandas as pd
+
 # import numpy as np
 from datetime import datetime
 
 
+def process_excel(target_excel):
+    data = pd.read_excel(target_excel, sheet_name=0)
+
+
 def loop_folder_excels():
-	path = os.getcwd()
-	directory = (datetime.today()).strftime('%Y-%m-%d')
-	path = path + '/' + directory + '/'
-	for filename in os.listdir(path):
-		if filename.endswith(".xlsx") or filename.endswith(".xls"):
-			print(path + filename)
+    path = os.getcwd()
+    directory = (datetime.today()).strftime("%Y-%m-%d")
+    path = path + "/" + directory + "/"
+    for filename in os.listdir(path):
+        if filename.endswith(".xlsx") or filename.endswith(".xls"):
+            print(path + filename)
 
 
 if __name__ == "__main__":
-	loop_folder_excels()
+    loop_folder_excels()
