00e2a76de2bfa86b879dd161805b50c3e3e934f6
greatabel <myreceiver2for2github@gmail.com>
2021-03-10 14:57:02 +0800

    requirement docs

---
+++ b9944_map_pcg/ass1_pcg.py
@@ -0,0 +1,14 @@
+'''
+
+https://mikecgreenblog.wordpress.com/2018/05/31/the-generative-design-in-minecraft-competition-gdmc/
+
+https://medium.com/inspired-to-program-%E3%85%82-%D9%88-%CC%91%CC%91/procedural-generation-in-python-7b75127b2f74
+
+'''
+
+def main():
+	''
+
+
+if __name__ == "__main__":
+	main()
