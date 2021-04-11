2d295a2450e40ae868e90edb57945217d1617405
greatabel <myreceiver2for2github@gmail.com>
2021-04-11 17:06:04 +0800

    twitter trend

---
+++ university_counselor/a11022_a9181_a9156_twitter_trend/data_visualization/senkufu.js
@@ -0,0 +1,4 @@
+var DATA = {
+'data2021':3640, 'data2020':2201,'total_sentiment_polarity':0.69, 'average_sentiment_polarity':0.02,'num_positive':808, 'num_neural':2323,'num_nagtive':606, 'data2021':3640 };
+var RELATED_WORDS = {'senkaku':36,'japan':19,'china':14,'senkaku_islands':5,'japanese territory':5,'coast guard':4,'tibet':4,'meiji':3,'africa':3,'takeshima':3, };
+var WHO_TWEETS = {'swa011':5,'ひろちい0':4,'🇯🇵日本 心太郎':4,'CCP-China Watch':2,'Caravaggio2016_疲れたおじさん':2,'@zhangfei98761 張飛':2, };
