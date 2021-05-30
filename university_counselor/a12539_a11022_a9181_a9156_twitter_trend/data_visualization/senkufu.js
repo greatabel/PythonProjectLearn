41a80ee4524b59dce18a322abfdb11234d7daccd
greatabel <myreceiver2for2github@gmail.com>
2021-05-30 11:08:17 +0800

    mv a12539

---
+++ university_counselor/a12539_a11022_a9181_a9156_twitter_trend/data_visualization/senkufu.js
@@ -0,0 +1,4 @@
+var DATA = {
+'data2021':1641, 'data2020':1824,'total_sentiment_polarity':1.24, 'average_sentiment_polarity':0.07,'num_positive':606, 'num_neural':1010,'num_nagtive':101, 'data2021':1641 };
+var RELATED_WORDS = {'senkaku':18,'japan':12,'china':9,'taiwan':4,'japanese territory':3,'tibet':2,'senkakuislands':2,'takeshima':2, };
+var WHO_TWEETS = {'ひろちい0':5,'name':1,'⛩️令和時代の日本を清らかに🎌':1,'Hans Solo':1,'Caravaggio2016_疲れたおじさん':1,'Xy5Z89🇩🇪🇪🇺':1,'フルベビアイス3':1,'swa011':1,'Donald Wong':1,'アナンケの白いお髭(Anankes white beard)':1, };
---
+++ university_counselor/a12539_a11022_a9181_a9156_twitter_trend/data_visualization/southchinasea.js
@@ -0,0 +1,4 @@
+var DATA = {
+'data2020':26818, 'data2019':30959,'total_sentiment_polarity':12.92, 'average_sentiment_polarity':0.05,'num_positive':8686, 'num_neural':12019,'num_nagtive':6161, 'data2019':30959 };
+var RELATED_WORDS = {'philippines':48,'sea https':29,'southchinasea':26,'beijing':18,'japan':12,'duterte':11,'u.s':8,'tibet':8,'asia':8,'australia':7, };
+var WHO_TWEETS = {'AMTI':8,'The Buried Lead - Ramon Ortoll':2,'South China Sea Connect':2,'OSINT-1':2,'Susan Raciti':2,'Narayan':2,'Oceanner':2, };
