6a6dc1ecc40314785e3659fc68a0c086b9ee82df
greatabel <myreceiver2for2github@gmail.com>
2021-09-15 01:56:43 +0800

    mv b14117 black

--- university_counselor/a9156_twitter_trend/data_visualization/southchinasea.js
+++ university_counselor/a9156_twitter_trend/data_visualization/southchinasea.js
@@ -1,4 +1,4 @@
 var DATA = {
-'data2020':8092, 'data2019':42480,'total_sentiment_polarity':2.78, 'average_sentiment_polarity':0.03,'num_positive':3232, 'num_neural':3636,'num_nagtive':1313, 'data2019':42480 };
+'data2020':8092, 'data2019':42480,'total_sentiment_polarity':2.78, 'average_sentiment_polarity':0.03,'num_positive':4232, 'num_neural':5636,'num_nagtive':913, 'data2019':42480 };
 var RELATED_WORDS = {'india':10,'sea https':8,'southchinasea':6,'beijing':5,'asean':4,'america':4,'uk':4,'elizabeth':4,'germany':4,'august':4, };
 var WHO_TWEETS = {'AMTI':4,'GENERAL TOM S. GATES ★ Live Leak 🦅🇺🇸':3, };
