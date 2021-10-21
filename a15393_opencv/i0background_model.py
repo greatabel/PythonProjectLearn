f88a9d7dd69f3c15a3879f103a5aaa0741b6cddc
greatabel <myreceiver2for2github@gmail.com>
2021-10-21 18:17:29 +0800

    task1

--- a15393_opencv/i0background_model.py
+++ a15393_opencv/i0background_model.py
@@ -1,1 +1,26 @@
 import cv2
+import numpy as np
+
+cam = cv2.VideoCapture('video/carpark_f6_compressed.avi')
+
+# Gaussian Mixture-based Background
+backSub = cv2.createBackgroundSubtractorMOG2()
+
+# Taking a matrix of size 5 as the kernel
+kernel = np.ones((5,5), np.uint8)
+
+while True:
+	_, img = cam.read()
+	fgMask = backSub.apply(img)
+	img_erosion = cv2.erode(fgMask, kernel, iterations=1)
+	img_dilation = cv2.dilate(img_erosion, kernel, iterations=1)
+	cv2.imshow('FG Mask', img_dilation)
+
+ 
+
+	key = cv2.waitKey(30) & 0xff
+	if key == 27:
+		break
+ 
+cam.release()
+cv2.destroyAllWindows()
