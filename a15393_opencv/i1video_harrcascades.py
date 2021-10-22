6bac24bd79643284834a76fd6e36ee55262dfce3
greatabel <myreceiver2for2github@gmail.com>
2021-10-22 17:00:05 +0800

    finished part2

--- a15393_opencv/i1video_harrcascades.py
+++ a15393_opencv/i1video_harrcascades.py
@@ -19,6 +19,7 @@ while True:
     img_1 = img.copy()
     img_2 = img.copy()
     img_3 = img.copy()
+    img_4 = img.copy()
     plt.imshow(img_1)
 
     gray_img = cv2.cvtColor(img_2, cv2.COLOR_BGR2GRAY)
@@ -28,17 +29,32 @@ while True:
 
     scale = 5.0
     bcolor = (255, 0, 0)
+    bcolor_j = (255, 255, 0)
     # faces = face_haar.detectMultiScale(gray_img)
     i = 0
+
+    j = 0
+    myareas = []
+    for face_x,face_y,face_w,face_h in faces:
+        myareas.append(face_w*face_h)
+    myareas.sort(reverse=True)
+    print('myareas=', myareas)
+    # find top 5 max area if not up to 5, then last area
+    threshold_area = 0
+    if len(myareas) >= 5:
+        threshold_area = myareas[4]
+
+
     for face_x,face_y,face_w,face_h in faces:
         cv2.rectangle(img_2, (face_x, face_y), (face_x+face_w, face_y+face_h), (0,255,0), 2)
         cv2.rectangle(img_3, (face_x, face_y), (face_x+face_w, face_y+face_h), (0,255,0), 2)
+        cv2.rectangle(img_4, (face_x, face_y), (face_x+face_w, face_y+face_h), (0,255,0), 2)
  
         roi_gray_img = gray_img[face_y:face_y+face_h, face_x:face_x+face_w]
         roi_img = img_2[face_y:face_y+face_h, face_x:face_x+face_w]
         cv2.putText(
             img_3,
-            "{:s} {:s}".format('pedestrian ', str(i)),
+            "{:s}-{:s}".format('P', str(i)),
             (face_x, face_y),
             cv2.FONT_HERSHEY_SIMPLEX,
             min(scale / 2, 2),
@@ -47,12 +63,29 @@ while True:
             lineType=cv2.LINE_AA,
         )
         i += 1
+
+        if j < 5 and face_w * face_h >= threshold_area:
+            cv2.putText(
+                img_4,
+                "{:s}_{:s}".format('P', str(j)),
+                (face_x, face_y),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                min(scale / 2, 5),
+                bcolor_j,
+                min(int(scale), 5),
+                lineType=cv2.LINE_AA,
+            )
+            j += 1
+
     plt.subplot(2, 2, 2)
     plt.imshow(img_2)
 
 
     plt.subplot(2, 2, 3)
     plt.imshow(img_3)
+
+    plt.subplot(2, 2, 4)
+    plt.imshow(img_4)
 
     # cv2.imshow('FG Mask', img_dilation)
     plt.show(block=False)
