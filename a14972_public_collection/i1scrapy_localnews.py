595d8d15b9a723f34d9589059bcf2fca9bc9a6e8
greatabel <myreceiver2for2github@gmail.com>
2021-11-08 00:03:29 +0800

    change name of a14972

---
+++ a14972_public_collection/i1scrapy_localnews.py
@@ -0,0 +1,31 @@
+import json
+import requests
+from lxml import etree
+
+data_folder = "data"
+
+headers = {
+    "User-Agent":"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36"
+}
+response = requests.get('https://www.baidu.com/s?wd=天津&lm=1', headers=headers)
+
+r = response.text
+html = etree.HTML(r, etree.HTMLParser())
+r1 = html.xpath('//h3')
+r2 = html.xpath('//*[@class="c-abstract"]')
+r3 = html.xpath('//*[@class="t"]/a/@href')
+
+for i in range(10):
+	if len(r1) > 0:
+	    r11 = r1[i].xpath('string(.)')
+	    r22 = r2[i].xpath('string(.)')
+	    r33 = r3[i]
+	    with open(data_folder+'/tianjin.txt', 'a', encoding='utf-8') as c:
+	         c.write(json.dumps(r11,ensure_ascii=False) + '\n')
+	         c.write(json.dumps(r22, ensure_ascii=False) + '\n')
+	         c.write(json.dumps(r33, ensure_ascii=False) + '\n')
+	         c.write('------------------------\n')
+	    print(r11, end='\n')
+	    print('------------------------')
+	    print(r22, end='\n')
+	    print(r33)
--- a15264_file_search_engine/README.md
+++ a15264_file_search_engine/README.md
@@ -20,4 +20,24 @@ pip install --upgrade -r requirements.txt
 
 5.
 模拟运行在:
-python3 main.py
+python3 wsgi.py
+
+
+
+6.
+浏览器访问：
+
+http://localhost:5000/home
+
+已经注册好的账号 可以直接登录：
+username: greatabel1@126.com 
+password: abel
+你也可以自己注册和登录
+
+7.
+个人主页： http://localhost:5000/profile
+
+修改编辑地点的后台管理页：
+http://localhost:5000/blogs
+可以点击 add doc连接，自己从pdf上copy文字输入到列表中；
+或者命令行下：python3 i0ocr.py (记得修改14行的文件名，获得processeddata的text，目前60-70%准确度)然后再复制黏贴
