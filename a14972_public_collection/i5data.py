7713be043dd753b6dfc4fd93c6132c5a40f869aa
greatabel <myreceiver2for2github@gmail.com>
2021-11-25 16:37:45 +0800

    add big_screen

--- a14972_public_collection/i5data.py
+++ a14972_public_collection/i5data.py
@@ -3,31 +3,27 @@ import json
 class SourceDataDemo:
 
     def __init__(self):
-        self.title = '大数据可视化展板通用模板'
-        self.counter = {'name': '2018年总收入情况', 'value': 12581189}
-        self.counter2 = {'name': '2018年总支出情况', 'value': 3912410}
+        self.title = '舆情大数据可视化展板'
+        self.counter = {'name': 'twitter中文圈讨论中国经济', 'value': 250}
+        self.counter2 = {'name': '环球政治军事数据', 'value': 310}
         self.echart1_data = {
-            'title': '行业分布',
+            'title': 'twitter情绪分布',
             'data': [
-                {"name": "商超门店", "value": 47},
-                {"name": "教育培训", "value": 52},
-                {"name": "房地产", "value": 90},
-                {"name": "生活服务", "value": 84},
-                {"name": "汽车销售", "value": 99},
-                {"name": "旅游酒店", "value": 37},
-                {"name": "五金建材", "value": 2},
+                {"name": "num_positive", "value": 120},
+                {"name": "num_neural", "value": 30},
+                {"name": "num_nagtive", "value": 100},
+
             ]
         }
         self.echart2_data = {
-            'title': '省份分布',
+            'title': 'twitter中文圈关联词分布',
             'data': [
-                {"name": "浙江", "value": 47},
-                {"name": "上海", "value": 52},
-                {"name": "江苏", "value": 90},
-                {"name": "广东", "value": 84},
-                {"name": "北京", "value": 99},
-                {"name": "深圳", "value": 37},
-                {"name": "安徽", "value": 150},
+                {"name": "national gallery", "value": 10},
+                {"name": "art", "value": 4},
+                {"name": "中国崛起", "value": 3},
+                {"name": "南海", "value": 3},
+                {"name": "australia", "value": 2},
+       
             ]
         }
         self.echarts3_1_data = {
@@ -47,59 +43,53 @@ class SourceDataDemo:
                 {"name": "教育", "value": 20},
                 {"name": "IT/互联网", "value": 20},
                 {"name": "金融", "value": 30},
-                {"name": "学生", "value": 40},
+                {"name": "freelancer", "value": 40},
                 {"name": "其他", "value": 50},
             ]
         }
         self.echarts3_3_data = {
             'title': '兴趣分布',
             'data': [
-                {"name": "汽车", "value": 4},
-                {"name": "旅游", "value": 5},
-                {"name": "财经", "value": 9},
-                {"name": "教育", "value": 8},
-                {"name": "软件", "value": 9},
-                {"name": "其他", "value": 9},
+                {"name": "中国艺术", "value": 4},
+                {"name": "中国文化", "value": 5},
+                {"name": "中国经济", "value": 9},
+                {"name": "中国军事", "value": 8},
             ]
         }
         self.echart4_data = {
             'title': '时间趋势',
             'data': [
-                {"name": "安卓", "value": [3, 4, 3, 4, 3, 4, 3, 6, 2, 4, 2, 4, 3, 4, 3, 4, 3, 4, 3, 6, 2, 4, 4]},
-                {"name": "IOS", "value": [5, 3, 5, 6, 1, 5, 3, 5, 6, 4, 6, 4, 8, 3, 5, 6, 1, 5, 3, 7, 2, 5, 8]},
+                {"name": "环球新闻数", "value": [3, 4, 3, 4, 3, 4, 3, 6, 2, 4, 2, 4, 3, 4, 3, 4, 3, 4, 3, 6, 2, 4, 4]},
+                {"name": "本地天津新闻数", "value": [5, 3, 5, 6, 1, 5, 3, 5, 6, 4, 6, 4, 8, 3, 5, 6, 1, 5, 3, 7, 2, 5, 8]},
             ],
             'xAxis': ['01', '02', '03', '04', '05', '06', '07', '08', '09', '11', '12', '13', '14', '15', '16', '17',
                       '18', '19', '20', '21', '22', '23', '24'],
         }
         self.echart5_data = {
-            'title': '省份TOP',
+            'title': '新闻地域分布',
             'data': [
-                {"name": "浙江", "value": 2},
-                {"name": "上海", "value": 3},
-                {"name": "江苏", "value": 3},
-                {"name": "广东", "value": 9},
-                {"name": "北京", "value": 15},
-                {"name": "深圳", "value": 18},
-                {"name": "安徽", "value": 20},
-                {"name": "四川", "value": 13},
+                {"name": "天津", "value": 19},
+                {"name": "美国", "value": 12},
+                {"name": "欧洲", "value": 10},
+                {"name": "其他", "value": 9},
             ]
         }
         self.echart6_data = {
-            'title': '一线城市情况',
+            'title': '新闻地域分布比例',
             'data': [
-                {"name": "浙江", "value": 80, "value2": 20, "color": "01", "radius": ['59%', '70%']},
-                {"name": "上海", "value": 70, "value2": 30, "color": "02", "radius": ['49%', '60%']},
-                {"name": "广东", "value": 65, "value2": 35, "color": "03", "radius": ['39%', '50%']},
-                {"name": "北京", "value": 60, "value2": 40, "color": "04", "radius": ['29%', '40%']},
-                {"name": "深圳", "value": 50, "value2": 50, "color": "05", "radius": ['20%', '30%']},
+                {"name": "天津", "value": 64, "value2": 36, "color": "01", "radius": ['59%', '70%']},
+                {"name": "美国", "value": 76, "value2": 24, "color": "02", "radius": ['49%', '60%']},
+                {"name": "欧洲", "value": 80, "value2": 20, "color": "03", "radius": ['39%', '50%']},
+                {"name": "其他", "value": 82, "value2": 18, "color": "04", "radius": ['29%', '40%']},
+               
             ]
         }
         self.map_1_data = {
             'symbolSize': 100,
             'data': [
-                {'name': '海门', 'value': 239},
-                {'name': '鄂尔多斯', 'value': 231},
-                {'name': '招远', 'value': 203},
+                {'name': '天津', 'value': 239},
+                {'name': '北京', 'value': 121},
+                {'name': '其他', 'value': 203},
             ]
         }
 
@@ -202,48 +192,5 @@ class SourceData(SourceDataDemo):
         按照 SourceDataDemo 的格式覆盖数据即可
         """
         super().__init__()
-        self.title = '大数据可视化展板通用模板'
+        self.title = '舆情大数据可视化展板'
 
-class CorpData(SourceDataDemo):
-
-    def __init__(self):
-        """
-        按照 SourceDataDemo 的格式覆盖数据即可
-        """
-        super().__init__()
-        with open('corp.json', 'r', encoding='utf-8') as f:
-            data = json.loads(f.read())
-        self.title = data.get('title')
-        self.counter = data.get('counter')
-        self.counter2 = data.get('counter2')
-        self.echart1_data = data.get('echart1_data')
-        self.echart2_data = data.get('echart2_data')
-        self.echarts3_1_data = data.get('echarts3_1_data')
-        self.echarts3_2_data = data.get('echarts3_2_data')
-        self.echarts3_3_data = data.get('echarts3_3_data')
-        self.echart4_data = data.get('echart4_data')
-        self.echart5_data = data.get('echart5_data')
-        self.echart6_data = data.get('echart6_data')
-        self.map_1_data = data.get('map_1_data')
-
-class JobData(SourceDataDemo):
-
-    def __init__(self):
-        """
-        按照 SourceDataDemo 的格式覆盖数据即可
-        """
-        super().__init__()
-        with open('job.json', 'r', encoding='utf-8') as f:
-            data = json.loads(f.read())
-        self.title = data.get('title')
-        self.counter = data.get('counter')
-        self.counter2 = data.get('counter2')
-        self.echart1_data = data.get('echart1_data')
-        self.echart2_data = data.get('echart2_data')
-        self.echarts3_1_data = data.get('echarts3_1_data')
-        self.echarts3_2_data = data.get('echarts3_2_data')
-        self.echarts3_3_data = data.get('echarts3_3_data')
-        self.echart4_data = data.get('echart4_data')
-        self.echart5_data = data.get('echart5_data')
-        self.echart6_data = data.get('echart6_data')
-        self.map_1_data = data.get('map_1_data')
