b7cdac6359a7bcb6440ff346c537e0a189e4cb22
greatabel <myreceiver2for2github@gmail.com>
2022-01-26 14:08:33 +0800

    b17608 finished

---
+++ university_counselor/b17608music_situation_change/i1play_music.py
@@ -0,0 +1,14 @@
+import simpleaudio as sa
+
+wave_obj = sa.WaveObject.from_wave_file('data/1s.wav')
+# load the file
+
+# wave_obj = sa.WaveObject.from_wave_file("alarm.wav")
+play_obj = wave_obj.play()
+#play_obj.wait_done() #blocking call
+while True:
+    if(play_obj.is_playing()):
+        print('Playing')
+    else:
+        print('Ended')
+        break
