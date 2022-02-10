7d7a5f501bdecdaf06c24823f665e9b37e8ae22f
greatabel <myreceiver2for2github@gmail.com>
2022-02-11 01:08:28 +0800

    i14gui_of_iot.py

---
+++ university_counselor/a17382_rabbitmq_smarthomeII/i14gui_of_iot.py
@@ -0,0 +1,24 @@
+from threading import Timer
+import tkinter as tk
+def hello():
+    global k,t
+    k+=1
+    label['text']=str(k)
+    t=Timer(1, hello)
+    t.setDaemon(True)
+    t.start()
+
+def do_job():
+    t.cancel()         
+
+root = tk.Tk()
+root.geometry("300x100")
+k=0
+label=tk.Label(root,text='0',bd='5',fg='red',font=("Arial",15))
+label.place(x=10,y=5,width=40,height=30)
+button1=tk.Button(root,text='停止',command=do_job,fg='red',font=("Arial",15))
+button1.place(x=100,y=5,width=60,height=30)
+t = Timer(1, hello) 
+t.start()
+root.mainloop()	
+
