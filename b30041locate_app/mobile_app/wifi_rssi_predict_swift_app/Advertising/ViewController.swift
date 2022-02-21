44ee7694ee2f308d8b40d64c24f722358b547d09
greatabel <myreceiver2for2github@gmail.com>
2022-02-22 02:49:06 +0800

    finish b30041

--- b30041locate_app/mobile_app/wifi_rssi_predict_swift_app/Advertising/ViewController.swift
+++ b30041locate_app/mobile_app/wifi_rssi_predict_swift_app/Advertising/ViewController.swift
@@ -22,7 +22,31 @@ final class ViewController: UIViewController {
     sliderValueChanged()
   }
   
-  @IBAction func sliderValueChanged(_ sender: UISlider? = nil) {
+    @IBAction func test_this_position(_ sender: Any) {
+      let hub0 = Double(arc4random_uniform(125) + 1)
+      let hub1 = Double(arc4random_uniform(150) + 1)
+      let hub2 = Double(arc4random_uniform(180) + 1)
+      
+      let input = AdvertisingInput(hub0: hub0, hub1: hub1, hub2: hub2)
+
+      guard let output = try? advertising.prediction(input: input) else {
+        return
+      }
+
+      let rssi = output.rssi
+      /* according to i2visual_and_analysis.py results */
+      var distance = (-rssi + 37)/1.116
+      tvLabel.text = numberFormatter.string(from: hub0 as NSNumber)
+      radioLabel.text = numberFormatter.string(from: hub1 as NSNumber)
+      newspaperLabel.text = numberFormatter.string(from: hub2 as NSNumber)
+      
+      tvSlider.value = Float(hub0)
+      radioSlider.value = Float(hub1)
+      newspaperSlider.value = Float(hub2)
+      salesLabel.text = numberFormatter.string(from: distance as NSNumber)
+    }
+  
+    @IBAction func sliderValueChanged(_ sender: UISlider? = nil) {
     let hub0 = Double(tvSlider.value)
     let hub1 = Double(radioSlider.value)
     let hub2 = Double(newspaperSlider.value)
