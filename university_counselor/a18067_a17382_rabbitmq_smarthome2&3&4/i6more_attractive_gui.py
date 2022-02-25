9b5e503fc0c8790fa80ee7843f370aa6ab33a46f
greatabel <myreceiver2for2github@gmail.com>
2022-02-25 10:51:19 +0800

    add all

--- university_counselor/a18067_a17382_rabbitmq_smarthome2&3&4/i6more_attractive_gui.py
+++ university_counselor/a18067_a17382_rabbitmq_smarthome2&3&4/i6more_attractive_gui.py
@@ -6,17 +6,19 @@ import pickle
 from PIL import ImageTk, Image
 
 
-def hello():
-    global k, t
-    k += 1
-
+def load_data():
     # --------- share data -------
     fp = open("shared.pkl", "rb")
     shared = pickle.load(fp)
     r = shared["microwave"]
     print('GUI back thread is receving:', r)
     # --------- share data end -------
+    return r
 
+def hello():
+    global k, t
+    k += 1
+    r = load_data()
     label["text"] = str(k) + ':' + r
     t = Timer(1, hello)
     t.setDaemon(True)
@@ -30,12 +32,8 @@ if __name__ == "__main__":
     root = tk.Tk()
     root.geometry("600x300")
 
-
-    bg = ImageTk.PhotoImage(Image.open("images/microwave.png"))  
-
-
-
-
+    r0 = load_data()
+    bg = ImageTk.PhotoImage(Image.open("images/" + r + ".png"))  
     # Show image using label
     label1 = Label(root, image = bg)
     label1.place(x = 0,y = 0)
