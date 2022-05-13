c06f57a23e7fc49da1b9882db9bd67cb84886113
greatabel <myreceiver2for2github@gmail.com>
2022-05-14 00:59:50 +0800

    finish target website

--- a20001sql_injection_platform/i1target_website/final_target/app.py
+++ a20001sql_injection_platform/i1target_website/final_target/app.py
@@ -47,19 +47,25 @@ def scp2(index_id=""):
     return r
 
 
-# @app.route('/scp3/')
-# @app.route('/scp3/<index_id>')
-# def scp3(index_id=""):
-#     # input :http://www.baidu.com
-#     query_value = request.args.get('query')
-#     index =  " is here"
-#     r = make_response(
-#         render_template('scp3.html', query_value=query_value,index=index)
-#         )
-#     r.headers.set('Content-Security-Policy', "default-src * 'unsafe-inline'; connect-src 'self' 'nonce-987654321' ")
-#     return r
+@app.route('/scp3/')
+@app.route('/scp3/<index_id>')
+def scp3(index_id=""):
+    # input :http://www.baidu.com
+    query_value = request.args.get('query')
+    index =  " is here"
+
+    teacher_works = []
+    for t in query_db('select * from teacher_work'):
+        print(t[0],'#'*10, t[1])
+        teacher_works.append(t)
+
+    r = make_response(
+        render_template('scp3.html', query_value=query_value,index=index,teacher_works=teacher_works)
+        )
+    r.headers.set('Content-Security-Policy', "default-src * 'unsafe-inline'; connect-src 'self' 'nonce-987654321' ")
+    return r
 
 if __name__ == '__main__':
     # app.run()
-
+    print('#'*30, 'target website is working')
     app.run(host="localhost", port=8000, threaded=False)
